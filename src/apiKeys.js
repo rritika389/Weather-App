299247a636e265e1eefe23300b8336568aacabf3
Ritika Rathi <90149136+rritika389@users.noreply.github.com>
2023-07-18 22:41:34 +0530

    Add files via upload

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "59fdc914a36bd475d9c0944150fa8925",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
