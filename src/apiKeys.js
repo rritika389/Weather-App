f51ca9108ba9aaf5ab306165525bdae57cf9df82
Ritika Rathi <90149136+rritika389@users.noreply.github.com>
2024-01-25 17:08:31 +0530

    blank

--- src/apiKeys.js
+++ src/apiKeys.js
@@ -1,5 +1,1 @@
-// Visit https://api.openweathermap.org & then signup to get our API keys for free
-module.exports = {
-  key: "59fdc914a36bd475d9c0944150fa8925",
-  base: "https://api.openweathermap.org/data/2.5/",
-};
+
